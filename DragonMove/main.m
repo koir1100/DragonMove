0bb52dc4ee12af92d70c6a01d0bbb124780e7d83
Apple <apple@Apple-ui-iMac.local>
2013-05-29 16:05:00 +0900

    Initial Commit

---
+++ DragonMove/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DragonMove
+//
+//  Created by Apple on 13. 5. 29..
+//  Copyright __MyCompanyName__ 2013년. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
